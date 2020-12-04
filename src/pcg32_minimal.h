d0d2df7a76d9119131e437949b504c2fddfe1fae
zo <44142765+zoj613@users.noreply.github.com>
2020-12-04 11:09:12 +0200

    MAINT: convert splitmax and pcg32 code into header-only code

--- src/pcg32_minimal.h
+++ src/pcg32_minimal.h
@@ -11,6 +11,16 @@ typedef struct pcg32_rand {
     uint64_t inc;
 } pcg32_random_t;
 
-uint32_t pcg32_random_r(pcg32_random_t* rng);
+
+static inline uint32_t pcg32_random_r(pcg32_random_t* rng)
+{
+    uint64_t oldstate = rng->state;
+    // Advance internal state
+    rng->state = oldstate * 6364136223846793005ULL + (rng->inc|1);
+    // Calculate output function (XSH RR), uses old state for max ILP
+    uint32_t xorshifted = ((oldstate >> 18u) ^ oldstate) >> 27u;
+    uint32_t rot = oldstate >> 59u;
+    return (xorshifted >> rot) | (xorshifted << ((-rot) & 31));
+}
 
 #endif
