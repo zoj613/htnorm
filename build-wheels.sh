41d8369aae745e530d8103e6b52c188ab5f2caa8
zoj <44142765+zoj613@users.noreply.github.com>
2021-04-05 00:52:53 +0200

    Update build files and version number

--- build-wheels.sh
+++ build-wheels.sh
@@ -7,35 +7,38 @@ set -e -u -x
 cd $(dirname $0)
 
 bin_arr=(
-    /opt/python/cp36-cp36m/bin
-    /opt/python/cp37-cp37m/bin
+    #/opt/python/cp37-cp37m/bin
     /opt/python/cp38-cp38/bin
-    /opt/python/cp39-cp39/bin
+    #/opt/python/cp39-cp39/bin
 )
 
 # add  python to image's path
-export PATH=/opt/python/cp38-cp38/bin/:$PATH
+export PATH=/opt/python/cp37-cp37m/bin/:$PATH
 # download install script
 curl -#sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py > get-poetry.py
 # install using local archive
-python get-poetry.py -y --file poetry-1.1.4-linux.tar.gz
+python get-poetry.py -y --file poetry-1.1.5-linux.tar.gz
 # install openblas
 yum install -y openblas-devel
 
 function build_poetry_wheels
 {
-    # build wheels for 3.6-3.9 with poetry 
+    # build wheels for 3.7-3.9 with poetry 
     for BIN in "${bin_arr[@]}"; do
         rm -Rf build/*
         # install build deps
-        "${BIN}/python" ${HOME}/.poetry/bin/poetry run pip install numpy
+        "${BIN}/python" ${HOME}/.poetry/bin/poetry run pip install numpy==1.18.1
         BUILD_WHEELS=1 "${BIN}/python" ${HOME}/.poetry/bin/poetry build -f wheel
-        auditwheel repair dist/*.whl --plat $1
-        whl="$(basename dist/*.whl)"
-        "${BIN}/python" -m pip install wheelhouse/"$whl"
-        # test if installed wheel imports correctly
-        "${BIN}/python" -c "from pyhtnorm import *"
-        rm dist/*.whl
+        mkdir -p ./tmp
+        for whl in dist/*.whl; do
+            auditwheel repair "$whl" --plat $1 -w "./tmp" 
+            whlname="$(basename "$(echo ./tmp/*.whl)")"
+            "${BIN}/python" -m pip install ./tmp/"$whlname"
+            # test if installed wheel imports correctly
+            "${BIN}/python" -c "from pyhtnorm import *"
+            mv ./tmp/*.whl wheelhouse/
+            rm "$whl"
+        done
     done
 }
 
